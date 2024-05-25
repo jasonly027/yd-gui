d91f4b293e13cf254fcad661f0c0f1bc5919af05
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-05-25 14:13:03 -0700

    Add traits, ManagedVideo constructor

--- src/video.rs
+++ src/video.rs
@@ -1,17 +1,18 @@
 use std::sync::{atomic::AtomicBool, Arc};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, sqlx::FromRow)]
 pub struct VideoInfo {
     pub video_id: String,
     pub title: String,
     pub author: String,
     pub duration_seconds: String,
     pub thumbnail: Option<String>,
+    #[sqlx(skip)]
     pub video_formats: Vec<VideoFormat>,
     pub audio_available: bool,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, sqlx::FromRow)]
 pub struct VideoFormat {
     pub container: String,
     pub width: String,
@@ -20,9 +21,24 @@ pub struct VideoFormat {
 }
 
 #[derive(Debug, Clone)]
-struct ManagedVideo {
-    id: i64,
+pub struct ManagedVideo {
+    id: i32,
     video_info: VideoInfo,
     content_size: Option<u64>,
     downloading: Arc<AtomicBool>,
+}
+
+impl ManagedVideo {
+    pub fn new(id: i32, video_info: VideoInfo) -> Self {
+        Self {
+            id,
+            video_info,
+            content_size: None,
+            downloading: Arc::new(AtomicBool::new(false))
+        }
+    }
+
+    pub fn get_info(&self) -> &VideoInfo {
+        &self.video_info
+    }
 }
